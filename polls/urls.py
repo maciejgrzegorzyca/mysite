58f321982c19d51205d47a4d2e40b1e133c75619
Maciej Grzegorzyca <maciej.grzegorzyca@users.notreply.github.com>
2023-03-06 20:07:02 +0100

    Register Choice class in polls/admin

--- polls/urls.py
+++ polls/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /added the qord "surveys" instead of polls
+    # ex: /added the word 'survey' instead of polls
     # ex.survey/5/ instead of polls/5
     # the 'name' value as called by the {% url %} template tag
     path('survey/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
